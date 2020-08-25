92149e5dc6fd8af66232753285e7935873ecef5a
Ebubekir Bastama <ebubekirbastama@elmalicesmekuruyemis.com>
2020-08-25 17:25:39 +0300

    Update telnet.py

--- telnet.py
+++ telnet.py
@@ -1,25 +1,22 @@
-import socket
-
-
-target_host = "ipadresi"
-target_port = port
-
-s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-s.connect((target_host, target_port))
-
-
-for x in range(1001):
-    response = s.send(s)
-    response = s.recv(4096)
-    print(response)
-
-
-
-
-
-
-
-
-
-
+from socket
+import socket from telnetlib
+import Telnet
+import time
+import re 
+a = 0 
+sock = socket() 
+sock.connect(('ipadresi', 19001)) 
+    print(sock.recv(256)) 
+data = sock.recv(256) 
+    print(data) 
+last = data.decode().split("\n")[1] 
+    print(last) lastt = last + '\n'
+sock.send(lastt.encode()) a += 1
+for i in range(1001): data = sock.recv(256) 
+    print(data)
+    last = data.decode() 
+    lastt = re.sub("[^A-Za-z0-9 ' &]+", '', last) 
+    lastt = lastt + '\n'
+sock.send(lastt.encode()) 
+a += 1 print(a) 
+time.sleep(0.9)
